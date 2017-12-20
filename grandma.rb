24b72a83dedb0e564087b8ae44c36f54e75c1da1
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-20 14:43:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,5 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.uppercase
-    puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else
-      puts "HUH?! SPEAK UP, SONNY!"
-  end
+
 end
