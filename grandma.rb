a4ccd00a677061cb08d072ff5f28412be4194c78
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-20 14:27:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -14,5 +14,6 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    "HUH?! SPEAK UP, SONNY!""
+    "HUH?! SPEAK UP, SONNY!"
+  end  
 end
