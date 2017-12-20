da64d780f6d3ba31c0a804ecd9a65e865e743b87
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-20 14:32:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
   else
-      "HUH?! SPEAK UP, SONNY!"  
+      "HUH?! SPEAK UP, SONNY!"
   end
 end
