e0121e0a19bd9ee4a1b5114440747306382b9275
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-20 14:50:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
 
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.uppercase
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   else
       "HUH?! SPEAK UP, SONNY!"
